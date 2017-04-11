b85892456f3b420940dadd074ff58298471f4a49
IDEALEVF <ideale58cf@gmail.com>
2017-04-11 13:53:34 +0200

    Add files via upload

---
+++ Couple.java
@@ -0,0 +1,28 @@
+package dash;
+import java.util.*;
+
+public class Couple {
+private int[] depart;
+private List chemin;
+
+public int[] getDepart() {
+	return depart;
+}
+
+public void setDepart(int[] depart) {
+	this.depart = depart;
+}
+
+public List getChemin() {
+	return chemin;
+}
+
+public void setChemin(List chemin) {
+	this.chemin = chemin;
+}
+
+public Couple(int[] depart, List prevchemin) {
+	this.depart = depart;
+	this.chemin = prevchemin;
+}
+}
