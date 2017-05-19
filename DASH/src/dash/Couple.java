583a459a5873a4cdf7ad84179484a1ac02db4fa0
Adélie CALVO FERNANDEZ <ideale58cf@gmail.com>
2017-05-19 15:47:57 +0200

    junit

--- DASH/src/dash/Couple.java
+++ DASH/src/dash/Couple.java
@@ -21,7 +21,15 @@ public class Couple {
 	public void setChemin(List chemin) {
 		this.chemin = chemin;
 	}
-
+	/**
+	 * 
+	 * Constructeur de Couple pour évolué2 (parcours en profondeur)
+	 * 
+	 * @param depart
+	 *            position actuelle
+	 * @param prevchemin
+	 *            chemin precedent
+	 */
 	public Couple(int[] depart, List prevchemin) {
 		this.depart = depart;
 		this.chemin = prevchemin;
